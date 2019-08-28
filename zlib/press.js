b17aea84b8580f056799336beffe7e29be820b0d
kurisu1901 <konyahoshi@qq.com>
2019-08-28 21:38:44 +0800

    使用gzip来压缩html

--- zlib/press.js
+++ zlib/press.js
@@ -6,5 +6,21 @@
  */
 const zlib = require('zlib')
 const fs = require('fs')
-const fileStream = fs.createReadStream('./example.txt')
-const
+const http = require('http')
+
+
+const server = http.createServer((req, res) => {
+  let acceptEncoding = req.headers['accept-encoding']
+  const path = __dirname + '/index.html'
+  fs.createReadStream(path).pipe(res);
+  return
+  if (acceptEncoding.indexOf('gzip') !== -1) {
+    let gzip = zlib.createGzip()
+    res.writeHead(200, {'content-encoding': 'gzip'})
+    fs.createReadStream(path).pipe(gzip).pipe(res)
+  } else {
+    fs.createReadStream(path).pipe(res);
+  }
+})
+
+server.listen(3000)
