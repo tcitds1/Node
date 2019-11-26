cc7641a8a9a39f352fc71f69b765f73ddc66ffe5
wenxiang_yi <konyahoshi@qq.com>
2019-11-26 21:04:36 +0800

    1

---
+++ struct/Stack.java
@@ -0,0 +1,20 @@
+public class Stack {
+    private String[] items;
+    private int n;
+    private int count; // 几个
+    public ArrayStack(n) {
+        items = new String[n];
+        this.n = n;
+        this.count = count;
+    }
+    public void push(String item) {
+        if (this.n == this.count) return
+        this.items[count] = item
+        this.count ++
+    }
+    public String pop(String item) {
+        if (this.count == 0) {
+            
+        }
+    }
+}
