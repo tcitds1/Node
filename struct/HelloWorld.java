0045e0c5a29135dac714b0f48634892d09257780
wenxiang_yi <konyahoshi@qq.com>
2019-12-08 15:02:38 +0800

    归并排序

---
+++ struct/HelloWorld.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+class HelloWorld {
+    public static void main(String[] argus) {
+        int[] arrays = {1,2,4,5,2,8,9};
+        int[] temp = new int[7];
+        merge(arrays, 0, 3, 7, temp);
+    }
+    public static int[] mergeSort(int[] arrays, int L, int R) {
+
+        
+    }
+    public static void merge(int[] arrays, int left, int middle, int right, int[] temp) {
+        int i = 0, j = middle + 1;
+        int llength = middle - left;
+        int rlenght = right - middle + 1;
+        int t = 0;
+        for (; (i <= llength && j <= right); ) {
+            if (arrays[i] < arrays[j]) {
+                temp[t++] = arrays[i++];
+            } else {
+                temp[t++] = arrays[j++];
+            }
+        }
+        while(i < llength) {
+            temp[t++] = arrays[i++];
+        }
+        while(j < right) {
+            temp[t++] = arrays[j++];
+        }
+        System.out.print(Arrays.toString(temp));
+    }
+}
--- struct/Sort.java
+++ struct/Sort.java
@@ -1,63 +1,45 @@
+// 归并排序算法, 氛围两部分
+// 1.分离，2.合并
+// 分离参数，array[], start, end, result[]
+// 合并参数，array[], start1, end1, start2, end2, result[]
+
 import java.util.Arrays;
 class Sort {
-  public void bubble (int[] array, int n) {
-    for (int i = 0; i < n; n++) {
-      boolean mark = true;
-      for (int j = 0; j <n-1-i; j++) {
-        if (array[j] > array[j + 1]) {
-          int temp = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = temp;
-          mark = false;
-        }
-      }
-      if (mark) {
-        break;
-      }
+  static void merge_sort_recursive(int[] arr, int[] result, int start, int end) {
+    if (start >= end)
+      return;
+    int mid = (start + end) / 2;
+    int start1 = start, end1 = mid;
+    int start2 = mid + 1, end2 = end;
+    merge_sort_recursive(arr, result, start1, end1);
+    merge_sort_recursive(arr, result, start2, end2);
+    merge(arr, start1, end1, start2, end2, result);
+  }
+  public static void merge(int[] arr, int start1, int end1, int start2, int end2, int[] result) {
+    int k = start1;
+    int bk = start1;
+    while(start1 <= end1 && start2 <= end2) {
+      result[k++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+    }
+    while(start1 <= end1) {
+      result[k++] = arr[start1++];
+    }
+    while(start2 <= end2) {
+      result[k++] = arr[start2++];
+    }
+    while(bk <= end2) {
+      arr[bk] = result[bk];
+      bk++;
     }
   }
-
-  // 插入排序要点
-  public static int[] insertSort(int[] array, int n) {
-    if (n <= 1) {
-      return array;
-    }
-    for (int i = 1; i < n; i++) {
-      int value = array[i];
-      int j = i - 1;
-      for (; j >= 0; j--) {
-        if (array[j] > value) {
-          array[j + 1] = array[j];
-        } else {
-          break;
-        }
-      }
-      array[j + 1] = value;
-    }
-    return array;
+  public static void merge_sort(int[] arr) {
+    int len = arr.length;
+    int[] result = new int[len];
+    merge_sort_recursive(arr, result, 0, len - 1);
   }
-  
-
-  // 选择排序
-  // 1 2 3 4 5
-  public static int[] selectSort(int[] array, int n) {
-    for (int i = 0; i < n; i++) {
-      int minIndex = i;
-      for (int j = i + 1; j < n; j++) {
-        if (array[minIndex] > array[j]) {
-          minIndex = j;
-        }
-      }
-      int temp = array[i];
-      array[i] = array[minIndex];
-      array[minIndex] = temp;
-    }
-    return array;
-  }
-
-  public static void main(String[] argus) {
-    int arr[] = {1,5,6,2,4,7,2,10};
-    int[] result = selectSort(arr, 8);
-    System.out.println(Arrays.toString(result));
+  public static void main(String[] argus){
+    int[] array = {1,3,2,4,56,29,10,2140,432,13,22,12};
+    merge_sort(array);
+    System.out.println(Arrays.toString(array));
   }
 }
